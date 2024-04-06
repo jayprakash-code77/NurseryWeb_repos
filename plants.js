ec3fad890f1986fc9ae65292e76d931e45c8a615
Deepu Kesarwani <deepukesarwani38@gmail.com>
2024-04-06 21:43:40 +0530

    login page add

--- plants.js
+++ plants.js
@@ -1,6 +1,7 @@
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
 let header = document.querySelector('.header-3');
+let scrollTop = document.querySelector('.scroll-top');
 
 menu.addEventListener('click',() =>{
     menu.classList.toggle('fa-times');
@@ -15,6 +16,14 @@ window.onscroll = () =>{
     }else{
         header.classList.remove('active');
     }
+
+    if (window.scrollY > 250) {
+        scrollTop.style.display = 'initial';
+    } else {
+        scrollTop.style.display = 'none';
+    }
+    
+ 
 }
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -32,3 +41,31 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+
+let countDate = new Date('may 1 2024 00:00:00').getTime();
+
+function countDown(){
+    let now = new Date().getTime();
+    gap = countDate - now;
+
+    let second = 1000;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+
+    let d = Math.floor(gap/(day));
+    let h = Math.floor(gap % (day)/ hour);
+    let m = Math.floor(gap % (hour)/(minute));
+    let s = Math.floor(gap % (minute)/(second));
+
+    document.getElementById('day').innerText = d;
+    document.getElementById('hour').innerText = h;
+    document.getElementById('minute').innerText = m;
+    document.getElementById('second').innerText = s;
+}
+
+setInterval(function(){
+    countDown();
+},1000
+)
