520100953212382bf91d25a3e821c707f3b33086
Jaypraksh Maurya <jayprakashmcode7@gmail.com>
2024-08-17 16:02:18 +0530

    Up To Date Commit

--- GreenGarden/models/cart.js
+++ GreenGarden/models/cart.js
@@ -1,5 +1,63 @@
 const mongoose = require("../config/batabase");
+const Plant = require("./plant");
+const User = require("./user");
 
-const cartSchema  = mongoose.Schema ({
-    
+
+// Cart Item Schema
+const cartItemSchema = mongoose.Schema({
+    productId: {
+        type: mongoose.Schema.ObjectId,
+        ref: "Plant",
+        required: true
+    },
+    quantity: {
+        type: Number,
+        default: 1,
+        min: 1,
+        max: 100
+    },
+    price: {
+        type: Number,
+        min: 0,
+        required: true
+    }
+}, {
+    timestamps: true
 });
+
+// cart Schema 
+const cartSchema = mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: true
+    },
+    items: [cartItemSchema],
+    totalPrice: {
+        type: Number,
+        min: 0,
+        required: true
+    },
+    discountPrice: {
+        type: Number,
+        min: 0
+    },
+    finalPrice: {
+        type: Number,
+        min: 0,
+        required: true
+    }
+}, {
+    timestamps: true // Optional: adds createdAt and updatedAt timestamps
+});
+
+
+// Method to calculate total price and final price
+cartSchema.methods.calculatePrices = function () {
+    this.totalPrice = this.items.reduce((total, item) => total + (item.price * item.quantity), 0);
+    this.finalPrice = this.totalPrice - (this.discountPrice || 0);
+};
+
+const Cart = mongoose.model('Cart', cartSchema);
+
+module.exports = Cart;
